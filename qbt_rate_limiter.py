4e151decd1aff09cdec5d3ec5fa72dfc848dc3e5
Mahesh K <2036998+mkopnsrc@users.noreply.github.com>
2024-07-21 04:31:37 -0400

    added func comments and reorder import libs

--- qbt_rate_limiter.py
+++ qbt_rate_limiter.py
@@ -1,15 +1,18 @@
-import requests
-import xml.etree.ElementTree as ET
-from qbittorrentapi import Client
-import logging
-from time import sleep
-from dotenv import load_dotenv
 from os import environ
+from time import sleep
+import logging
+import xml.etree.ElementTree as ET
+import requests
+from qbittorrentapi import Client
+from dotenv import load_dotenv
 
 load_dotenv()
 
 def setup_logger():
-    #Creates a logger instance, sets logging levels, and attaches handlers for both console and file output.
+    """
+    Creates a logger instance, sets logging levels, and attaches handlers for both 
+    console and file output.
+    """
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
@@ -30,9 +33,9 @@ def setup_logger():
 logger = setup_logger()
 
 def get_plex_sessions(plex_host, plex_token):
-    #Retrieve the current streaming sessions from Plex.
     """
     Returns:
+    Retrieve the current streaming sessions from Plex.
     xml.etree.ElementTree.Element: The root element of the XML response from Plex API.
     """
     endpoint = f'https://{plex_host}/status/sessions?X-Plex-Token={plex_token}'
@@ -45,7 +48,9 @@ def get_plex_sessions(plex_host, plex_token):
         return None
 
 def mbps_to_bps(mbps):
-    #Convert Megabytes per second (MB/s) to Bytes per second (B/s).
+    """
+    Convert Megabytes per second (MB/s) to Bytes per second (B/s).
+    """
     try:
         bps = int(float(mbps) * 1024 * 1024)
         return bps if bps > 0 else -1
@@ -54,7 +59,9 @@ def mbps_to_bps(mbps):
         return -1
 
 def get_current_qbt_limits(client):
-    #Retrieve the current upload and download speed limits from qBittorrent.
+    """
+    Retrieve the current upload and download speed limits from qBittorrent.
+    """
     try:
         current_upload_limit = client.transfer_upload_limit()
         current_download_limit = client.transfer_download_limit()
@@ -66,7 +73,9 @@ def get_current_qbt_limits(client):
         return None, None
 
 def set_qbt_limits(client, upload_limit, download_limit):
-    #Set the upload and download speed limits in qBittorrent.
+    """
+    Set the upload and download speed limits in qBittorrent.
+    """
     try:
         client.transfer_set_upload_limit(upload_limit)
         client.transfer_set_download_limit(download_limit)
@@ -83,22 +92,24 @@ def set_qbt_limits(client, upload_limit, download_limit):
         logger.error(f"Failed to set speed limits in qBittorrent: {e}")
 
 def process_plex_sessions(root, client, upload_limit, download_limit):
-    #Process the Plex streaming sessions and set qBittorrent speed limits accordingly.
+    """
+    Process the Plex streaming sessions and set qBittorrent speed limits accordingly.
+    """
     stream_count = root.attrib.get('size')
     logger.debug(f"Plex stream counts: {stream_count}")
     if stream_count != "0":
         logger.info("Someone is streaming from Plex!")
         sessions = root.findall('./Video')
         for session in sessions:
-            attributes = session.attrib
-            user = session.find('./User').attrib['title']
-            player = session.find('./Player').attrib
-            stream_type = player.get('local')
-            device_name = player.get('title')
-            library = attributes.get('librarySectionTitle')
-            grandparent_title = attributes.get('grandparentTitle')
-            parent_title = attributes.get('parentTitle')
-            title = attributes.get('title')
+            attributes          = session.attrib
+            user                = session.find('./User').attrib['title']
+            player              = session.find('./Player').attrib
+            stream_type         = player.get('local')
+            device_name         = player.get('title')
+            library             = attributes.get('librarySectionTitle')
+            grandparent_title   = attributes.get('grandparentTitle')
+            parent_title        = attributes.get('parentTitle')
+            title               = attributes.get('title')
 
             if grandparent_title:
                 logger.info(f"User: {user}, Device: {device_name}, Library: {library}, Title: {grandparent_title} - {parent_title} - {title}")
@@ -110,13 +121,15 @@ def process_plex_sessions(root, client, upload_limit, download_limit):
         set_qbt_limits(client, 0, 0)
 
 def main():
-    # Main function to initialize environment variables, create qBittorrent client, and run the main loop
-    plex_host = environ.get("PLEX_HOST")
-    plex_token = environ.get("PLEX_TOKEN")
-    qbt_host = environ.get("QBT_HOST")
-    qbt_user = environ.get("QBT_USER")
-    qbt_pass = environ.get("QBT_PASS")
-    upload_limit_mbps = environ.get("UPLOAD_LIMIT_MBPS", "0")
+    """
+    Main function to initialize environment variables, create qBittorrent client, and run the main loop
+    """
+    plex_host           = environ.get("PLEX_HOST")
+    plex_token          = environ.get("PLEX_TOKEN")
+    qbt_host            = environ.get("QBT_HOST")
+    qbt_user            = environ.get("QBT_USER")
+    qbt_pass            = environ.get("QBT_PASS")
+    upload_limit_mbps   = environ.get("UPLOAD_LIMIT_MBPS", "0")
     download_limit_mbps = environ.get("DOWNLOAD_LIMIT_MBPS", "0")
 
     if not all([plex_host, plex_token, qbt_host, qbt_user, qbt_pass]):
@@ -124,8 +137,8 @@ def main():
         return
 
     client = Client(host=qbt_host, username=qbt_user, password=qbt_pass)
-    upload_limit = mbps_to_bps(upload_limit_mbps)
-    download_limit = mbps_to_bps(download_limit_mbps)
+    upload_limit    = mbps_to_bps(upload_limit_mbps)
+    download_limit  = mbps_to_bps(download_limit_mbps)
 
     while True:
         current_upload_limit, current_download_limit = get_current_qbt_limits(client)
@@ -135,4 +148,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
